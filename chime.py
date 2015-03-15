6581e6b4349d3e965b6529ab7dd8f379dcb0ed5f
Yuri Niyazov <yuri.niyazov@gmail.com>
2015-03-15 10:19:14 -0700

    Added up and down scripts

--- chime.py
+++ chime.py
@@ -1,4 +1,4 @@
-import objc, sys, random
+import objc, sys, random, inspect, os
 from Foundation import *
 from AppKit import *
 from PyObjCTools import AppHelper
@@ -16,10 +16,24 @@ def datetime_to_chime(dt):
   minute_range = find_in(mapping, dt.minute)
   quarter_hour_chime = mapping[minute_range]
   if quarter_hour_chime == 0:
-    return (["Submarine", "Blow", "Purr", "Hero", "Ping", "Glass"],
+    return (["Blow", "Purr", "Hero", "Ping", "Glass"],
             int(dt.strftime("%I")))
   else:
-    return (["Basso"], quarter_hour_chime)
+    return (["Submarine"], quarter_hour_chime)
+
+def base_url():
+  b = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+  return NSURL.fileURLWithPath_(b)
+
+def get_volume_down_script():
+  nsa = NSAppleScript.alloc()
+  s = NSURL.URLWithString_relativeToURL_("volume_down.scpt", base_url()).absoluteURL()
+  return nsa.initWithContentsOfURL_error_(s, None)[0]
+
+def get_volume_up_script():
+  nsa = NSAppleScript.alloc()
+  s = NSURL.URLWithString_relativeToURL_("volume_up.scpt", base_url()).absoluteURL()
+  return nsa.initWithContentsOfURL_error_(s, None)[0]
 
 class Chimey(NSObject):
   sound_names = ["Basso", "Blow", "Bottle", "Frog", "Funk", "Glass", "Hero",
@@ -27,6 +41,7 @@ class Chimey(NSObject):
   sounds = {}
   
   def applicationDidFinishLaunching_(self, notification):
+    get_volume_down_script().executeAndReturnError_(None)
     random.seed()
     for name in self.sound_names:
       self.sounds[name] = NSSound.soundNamed_(name)
@@ -37,7 +52,7 @@ class Chimey(NSObject):
 
   def play(self, previous):
     if len(self.sequence) == 1:
-      sound = self.sequence[0]
+      sound = self.sounds[self.sequence[0]]
     else:
       sound = previous
       while (sound == previous):
@@ -48,6 +63,7 @@ class Chimey(NSObject):
     if finishedPlaying:
       self.count -= 1
       if self.count <= 0:
+        get_volume_up_script().executeAndReturnError_(None)
         AppHelper.stopEventLoop()
       elif not self.play(sound):
         raise Exception("Error playing sound")
